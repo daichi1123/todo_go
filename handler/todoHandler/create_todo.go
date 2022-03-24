8347a094da3822f7fa91f0c67f86a2a54c52bf34
daichi1123 <badass.haruzion@gmail.com>
2022-03-24 22:21:39 +0900

    created create todo func

--- handler/todoHandler/create_todo.go
+++ handler/todoHandler/create_todo.go
@@ -1,7 +1,28 @@
-package handler
+package todoHandler
 
-import "fmt"
+import (
+	"go_portofolio/model"
+	"log"
+	"time"
+)
 
-func createTodo() {
-	fmt.Println("this is create_todo file")
+type Todo model.Todo
+
+func (t Todo) CreateTodo() (err error) {
+	// createTodo test
+	t.Content = "test todo"
+	t.UserId = 1
+	t.CreatedAt = time.Now()
+	// createTodo test
+
+	insertT := `insert into todos (
+		content, user_id, created_at) values (?, ?, ?)`
+
+	_, err = model.Db.Exec(insertT, t.Content, t.UserId, t.CreatedAt)
+	if err != nil {
+		log.Print(err)
+	}
+	defer model.Db.Close()
+
+	return
 }
