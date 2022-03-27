75450305176b0f2f45ead2f34ccca5f740c6848b
daichi1123 <badass.haruzion@gmail.com>
2022-03-27 15:39:17 +0900

    emailからユーザの情報を取得する

---
+++ handler/userHandler/session_user.go
@@ -0,0 +1,27 @@
+package userHandler
+
+import (
+	"fmt"
+	"go_portofolio/model"
+	"log"
+)
+
+type user model.User
+
+func GetUserByEmail(email string) (user User, err error) {
+	fmt.Println("ここは北")
+	user = User{}
+	const select_user = `SELECT id, uuid, name, email, password
+	from users where email = ?`
+	err = model.Db.QueryRow(select_user, email).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.Password)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return
+}
